791d62a8a4d5850a20b0bbd3b645a3b0d83aaecf
shanuka1198 <shanukaimantha0@gmail.com>
2025-03-15 13:27:31 +0530

    implemented auto generated serviceFee and calculation Total fee

--- src/main/java/com/carwash/S/I_car_wash/service/serviceImpl/AppointmentServiceImpl.java
+++ src/main/java/com/carwash/S/I_car_wash/service/serviceImpl/AppointmentServiceImpl.java
@@ -2,6 +2,8 @@ package com.carwash.S.I_car_wash.service.serviceImpl;
 
 import com.carwash.S.I_car_wash.Entity.*;
 import com.carwash.S.I_car_wash.dto.AppointmentResponseDTO;
+import com.carwash.S.I_car_wash.dto.ServiceDTO;
+import com.carwash.S.I_car_wash.dto.VehicleDTO;
 import com.carwash.S.I_car_wash.repository.AppointmentRepository;
 import com.carwash.S.I_car_wash.dto.AppointmentDTO;
 import com.carwash.S.I_car_wash.repository.ServiceRepository;
@@ -12,6 +14,7 @@ import com.carwash.S.I_car_wash.service.AppointmentService;
 import org.springframework.stereotype.Service;
 
 
+import java.util.IllegalFormatCodePointException;
 import java.util.List;
 
 
@@ -55,8 +58,20 @@ public class AppointmentServiceImpl implements AppointmentService {
         Vehicles vehicle = vehicleRepository.findById(appointmentDTO.getVehicleId())
                 .orElseThrow(() -> new RuntimeException("Vehicle not found with ID: " + appointmentDTO.getVehicleId()));
 
-        Double totalFee= (appointmentDTO.getServiceFee()*appointmentDTO.getDiscount())/100;
 
+
+
+        ServiceEntity service1=new ServiceEntity();
+        service1.setServiceType(service.getServiceType());
+
+        Vehicles vehicles=new Vehicles();
+        vehicles.setVehicleType(vehicle.getVehicleType());
+
+        Double totalFee= (serviceFee(service1,vehicles)*appointmentDTO.getDiscount())/100;
+
+        System.out.println(service1.getServiceType());
+        System.out.println(vehicles.getVehicleType());
+        System.out.println(serviceFee(service1,vehicles));
         // Create and save appointment
         Appointment appointment = new Appointment();
         appointment.setAppointmentDateTime(appointmentDTO.getAppointmentDateTime());
@@ -64,7 +79,7 @@ public class AppointmentServiceImpl implements AppointmentService {
         appointment.setUser(user);
         appointment.setService(service);
         appointment.setNote(appointmentDTO.getNote());
-        appointment.setServiceFee(appointmentDTO.getServiceFee());
+        appointment.setServiceFee(serviceFee(service1,vehicles));
         appointment.setDiscount(appointmentDTO.getDiscount());
         appointment.setTotalFee(appointment.getServiceFee()-totalFee);
 
@@ -122,16 +137,65 @@ public class AppointmentServiceImpl implements AppointmentService {
                 .orElseThrow(() -> new RuntimeException("Service not found with ID: " + appointmentDTO.getServiceId()));
         appointment.setService(service);
 
-        Double totalFee= (appointmentDTO.getServiceFee()*appointmentDTO.getDiscount())/100;
 
+
+
+        ServiceEntity service1=new ServiceEntity();
+        service1.setServiceType(service.getServiceType());
+
+        Vehicles vehicles=new Vehicles();
+        vehicles.setVehicleType(vehicle.getVehicleType());
+
+        Double totalFee= (serviceFee(service1,vehicles)*appointmentDTO.getDiscount())/100;
 
         appointment.setAppointmentDateTime(appointmentDTO.getAppointmentDateTime());
         appointment.setNote(appointmentDTO.getNote());
-        appointment.setServiceFee(appointmentDTO.getServiceFee());
+        appointment.setServiceFee(serviceFee(service1,vehicles));
         appointment.setDiscount(appointmentDTO.getDiscount());
         appointment.setTotalFee(appointment.getServiceFee()-totalFee);
 
         return appointmentRepository.save(appointment);
+    }
+
+    private Double serviceFee(ServiceEntity serviceDTO, Vehicles vehicleDTO){
+        if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.CAR && serviceDTO.getServiceType()== ServiceEntity.ServiceType.FULL_SERVICE){
+            return 35000.0;
+        } else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.CAR && serviceDTO.getServiceType()== ServiceEntity.ServiceType.BASIC_SERVICE) {
+            return 25000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.CAR && serviceDTO.getServiceType()== ServiceEntity.ServiceType.EXPRESS_SERVICE) {
+            return 40000.0;
+        } else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.VAN && serviceDTO.getServiceType()== ServiceEntity.ServiceType.FULL_SERVICE) {
+            return 40000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.VAN && serviceDTO.getServiceType()== ServiceEntity.ServiceType.BASIC_SERVICE) {
+            return 30000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.VAN && serviceDTO.getServiceType()== ServiceEntity.ServiceType.EXPRESS_SERVICE) {
+            return 45000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.BIKE && serviceDTO.getServiceType()== ServiceEntity.ServiceType.FULL_SERVICE) {
+            return 5000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.BIKE && serviceDTO.getServiceType()== ServiceEntity.ServiceType.BASIC_SERVICE) {
+            return 3000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.BIKE && serviceDTO.getServiceType()== ServiceEntity.ServiceType.EXPRESS_SERVICE) {
+            return 7000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.BUS && serviceDTO.getServiceType()== ServiceEntity.ServiceType.FULL_SERVICE) {
+            return 50000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.BUS && serviceDTO.getServiceType()== ServiceEntity.ServiceType.BASIC_SERVICE) {
+            return 40000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.BUS && serviceDTO.getServiceType()== ServiceEntity.ServiceType.EXPRESS_SERVICE) {
+            return 55000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.LORRY && serviceDTO.getServiceType()== ServiceEntity.ServiceType.FULL_SERVICE) {
+            return 50000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.LORRY && serviceDTO.getServiceType()== ServiceEntity.ServiceType.BASIC_SERVICE) {
+            return 40000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.LORRY && serviceDTO.getServiceType()== ServiceEntity.ServiceType.EXPRESS_SERVICE) {
+            return 55000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.SUV && serviceDTO.getServiceType()== ServiceEntity.ServiceType.FULL_SERVICE) {
+            return 53000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.SUV && serviceDTO.getServiceType()== ServiceEntity.ServiceType.BASIC_SERVICE) {
+            return 42000.0;
+        }else if (vehicleDTO.getVehicleType()== Vehicles.VehicleType.SUV && serviceDTO.getServiceType()== ServiceEntity.ServiceType.EXPRESS_SERVICE) {
+            return 55000.0;
+        }
+        return 0.0;
     }
 
 }
